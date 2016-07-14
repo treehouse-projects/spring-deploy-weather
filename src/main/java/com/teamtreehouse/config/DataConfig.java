38dbf95eea282bd0bac39b324b0a9c64cea4943a
Chris Ramacciotti <chris.j.rama@gmail.com>
2016-07-14 11:16:22 -0500

    Removed annotation for auto-detected properties file

--- src/main/java/com/teamtreehouse/config/DataConfig.java
+++ src/main/java/com/teamtreehouse/config/DataConfig.java
@@ -4,7 +4,6 @@ import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -16,7 +15,6 @@ import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.teamtreehouse.dao")
-@PropertySource("application.properties")
 public class DataConfig {
     @Autowired
     private Environment env;
